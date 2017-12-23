b3072a603baa5a083f38c1609e132510634c1983
matthreif <matthias@reif.id.au>
2017-12-23 20:54:36 +1100

    Handle create timer requests via non-persistent storage.

--- src/main/scala/au/id/reif/timekeeper/service/TimekeeperService.scala
+++ src/main/scala/au/id/reif/timekeeper/service/TimekeeperService.scala
@@ -1,9 +1,15 @@
 package au.id.reif.timekeeper.service
 
+import java.util.UUID
+import java.util.concurrent.TimeUnit.SECONDS
+
 import akka.http.scaladsl.server.{Directive0, Route}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json._
+
+import scala.collection.mutable
+import scala.concurrent.duration.{FiniteDuration, TimeUnit}
 
 trait TimerState {
   val json: String
@@ -13,32 +19,56 @@ case object Stopped extends TimerState { val json = "stopped"}
 
 case class TimerStateRequest(state: TimerState)
 
+object TimerId {
+  def generate: TimerId = TimerId(UUID.randomUUID().toString)
+}
+
 case class TimerId(id: String)
 
-case class Timer(id: TimerId, state: TimerState)
+case class Timer(id: TimerId, state: TimerState, elapsed: FiniteDuration)
 
 // collect your json format instances into a support trait:
-
-
 trait TimekeeperServiceJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val timerIdFormat: JsonFormat[TimerId] = jsonFormat1(TimerId.apply)
   implicit object timerStateFormat extends JsonFormat[TimerState] {
     override def read(json: JsValue): TimerState = json match {
       case JsString(Stopped.json) => Stopped
       case JsString(Running.json) => Running
-      case unknown => throw DeserializationException(s"Failed to deserialise $unknown")
+      case unknown => deserializationError(s"Failed to deserialise $unknown")
     }
 
     override def write(obj: TimerState): JsValue = JsString(obj.json)
   }
 
+  implicit object FiniteDurationJsonFormat extends RootJsonFormat[FiniteDuration]{
+    def write(dur: FiniteDuration) = JsObject(
+      "length" -> JsNumber(dur.length),
+      "unit" -> JsString(dur.unit.toString)
+    )
+    def read(value: JsValue) = {
+      value.asJsObject.getFields("length", "unit") match {
+        case Seq(JsNumber(length), JsString(unit)) => FiniteDuration(length.toLong, unit.toLowerCase)
+        case _ => deserializationError("FiniteDuration expected")
+      }
+    }
+  }
   implicit val timerStateRequestFormat: RootJsonFormat[TimerStateRequest] = jsonFormat1(TimerStateRequest.apply)
+  implicit val timerFormat: RootJsonFormat[Timer] = jsonFormat3(Timer.apply)
 }
 
 final class TimekeeperService extends TimekeeperServiceJsonSupport {
 
+  val database: mutable.Map[TimerId, Timer] = mutable.Map.empty
+
   def route: Route = getTimerRoute ~ deleteTimerRoute() ~ updateTimerRoute() ~ createTimerRoute
 
   def pathTimers: Directive0 = path("timers")
+
+  def handleCreateTimer(request: TimerStateRequest): Timer = {
+    val timer = Timer(TimerId.generate, request.state, FiniteDuration.apply(0, SECONDS))
+    database += timer.id -> timer
+    timer
+  }
 
   // API
 
@@ -49,7 +79,7 @@ final class TimekeeperService extends TimekeeperServiceJsonSupport {
   def createTimerRoute: Route = post {
     pathTimers {
       entity(as[TimerStateRequest]) { request =>
-        complete(s"Create timer with state ${request.state.json}" )
+        complete(handleCreateTimer(request))
       }
     }
   }
--- src/test/scala/au/id/reif/timekeeper/service/TimekeeperServiceTest.scala
+++ src/test/scala/au/id/reif/timekeeper/service/TimekeeperServiceTest.scala
@@ -1,21 +1,32 @@
 package au.id.reif.timekeeper.service
 
+import java.util.concurrent.TimeUnit.SECONDS
+
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.scalatest.{FunSuite, Matchers}
+
+import scala.concurrent.duration.FiniteDuration
 
 final class TimekeeperServiceTest extends FunSuite with Matchers with ScalatestRouteTest with TimekeeperServiceJsonSupport {
 
   test("POST with timer state should report the correct state") {
     val service = new TimekeeperService
+    val zeroSeconds = FiniteDuration(0, SECONDS)
 
     Post("/timers", TimerStateRequest(Running)) ~> service.route ~> check {
       handled shouldBe true
-      responseAs[String] shouldBe "Create timer with state running"
+      responseAs[Timer] should have (
+        'state (Running),
+        'elapsed (zeroSeconds)
+      )
     }
 
     Post("/timers", TimerStateRequest(Stopped)) ~> service.route ~> check {
       handled shouldBe true
-      responseAs[String] shouldBe "Create timer with state stopped"
+      responseAs[Timer] should have (
+        'state (Stopped),
+        'elapsed (zeroSeconds)
+      )
     }
   }
 }
